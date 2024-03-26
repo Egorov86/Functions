8d3902f85dfa55e1b4a8fb5ef608a63ed5c42918
Egoro_st25@top-academy.ru <Egoro_st25@top-academy.ru>
2024-03-26 14:30:32 +0300

    Arrays functions in progress...

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -1,128 +1,277 @@
 ﻿//Arrays
-#include <iostream>
-#include <string>
-#include <time.h>
+#include<iostream>
 using namespace std;
-void Print(int arr[], int n)
+
+#define tab "\t"
+
+void FillRand(int arr[], const int n, int minRand = 0, int maxRand = 100);
+void FillRand(double arr[], const int n, int minRand = 0, int maxRand = 100);
+void FillRand(char arr[], const int n, int minRand = 0, int maxRand = 100);
+
+void Print(const int arr[], const int n);
+void Print(const double arr[], const int n);
+void Print(const char arr[], const int n);
+
+int Sum(const int arr[], const int n);
+int Sum(const double arr[], const int n);
+
+double Avg(const int arr[], const int n);
+double Avg(const double arr[], const int n);
+
+int minValueIn(const int arr[], const int n);
+int maxValueIn(const int arr[], const int n);
+int minValueIn(const double arr[], const int n);
+int maxValueIn(const double arr[], const int n);
+
+void shiftleft(int arr[], const int n);
+void shiftRight(int arr[], const int n);
+void shiftleft(double arr[], const int n);
+void shiftRight(double arr[], const int n);
+
+void main()
 {
-    cout << "Вывод массива  { ";
-    for (int i = 0; i < n; i++)
-    {
-        cout <<  arr[i] << " ";
-    }
-    cout << "}";
-    cout << endl;
+	setlocale(LC_ALL, "");
+	const int n = 5;
+	int arr[n];
+	/*int minRand, maxRand;
+	do
+	{
+		cout << "Введите мин возможное случ.число: ";	cin >> minRand;
+		cout << "Введите макс возможное случ.число: ";	cin >> maxRand;
+		if (minRand == maxRand)cout << "Пределы не должны совпадать" << endl;
+
+	} while (minRand == maxRand);*/
+
+	FillRand(arr, n);
+	Print(arr, n);
+	cout << "Сумма элементов массива: " << Sum(arr, n) << endl;
+	cout << "Среднее-арифметическое элементов массива: " << Avg(arr, n) << endl;
+	cout << "Минимальное значение элементов массива: " << minValueIn(arr, n) << endl;
+	cout << "Максимальное значение элементов массива: " << maxValueIn(arr, n) << endl;
+	shiftleft(arr, n);
+	cout  << endl;
+	shiftRight(arr, n);
+	
 }
-void FillRand(int arr[], int n, int minRand, int maxRand)
+
+void FillRand(int arr[], const int n, int maxRand, int minRand)
 {
-    for (int i = 0; i < n; i++)
-    {
-        arr[i] = minRand + rand() % (maxRand - minRand);
-    }
-    cout << endl;
+	if (maxRand < minRand)
+	{
+		int buffer = minRand;
+		minRand = maxRand;
+		maxRand = buffer;
+	}
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = minRand + rand() % (maxRand - minRand);
+	}
+
 }
-int sum;
-void Sum(int arr[], int n)
+void FillRand(double arr[], const int n, int maxRand, int minRand)
 {
-    for (int i = 0; i < n; i++)
-    {
-        sum += arr[i];
-    }
+	if (maxRand < minRand)
+	{
+		int buffer = minRand;
+		minRand = maxRand;
+		maxRand = buffer;
+	}
+	minRand *= 100;
+	maxRand *= 100;
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = minRand + rand() % (maxRand - minRand);
+		arr[i] /= 100;
+	}
+
 }
-void vivodSum(int arr[], int n)
+void FillRand(char arr[], const int n, int maxRand, int minRand)
 {
-    for (int i = 0; i < 1; i++)
-    {
-        cout << "Cумма массива = " << sum << " ";
-    }
-    cout << endl;
+	if (maxRand < minRand)
+	{
+		int buffer = minRand;
+		minRand = maxRand;
+		maxRand = buffer;
+	}
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = minRand + rand() % (maxRand - minRand);
+	}
+
 }
-double c;
-int Avg(int arr[], int n)
+
+void Print(const int arr[], const int n)
 {
-    for (int i = 0; i < 1; i++)
-    {
-        return c = double(sum) / n;
-    }
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
 }
-void vivodAvg(int arr[], int n)
+void Print(const double arr[], const int n)
 {
-    for (int i = 0; i < 1; i++)
-    {
-        cout << "Cреднее массива = " << c << " ";
-    }
-    cout << endl;
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
 }
-int minValueIn(int arr[], int n)
+void Print(const char arr[], const int n)
 {
-    for (int i = 0; i < n; i++)
-    {
-        if (arr[i] > arr[0])
-        {
-            int min = arr[0];
-            arr[0] = arr[i];
-            arr[i] = min;
-            return min;
-        }
-    }
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
 }
-void vivodminValueIn(int arr[], int n)
+
+int Sum(const int arr[], const int n)
 {
-    for (int i = 0; i < 1; i++)
-    {
-        cout << "Минимальное знач. массива = " << minValueIn(arr, n) << " ";
-    }
-    cout << endl;
+	int sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		sum += arr[i];
+	}
+	return sum;
 }
-int maxValueIn(int arr[], int n)
+int Sum(const double arr[], const int n)
 {
-    for (int i = 0; i < n; i++)
-    {
-        if (arr[i] < arr[i - 1])
-        {
-            int max = arr[i - 1];
-            arr[i - 1] = arr[i];
-            arr[i] = max;
-            return max;
-        }
-    }
+	int sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		sum += arr[i];
+	}
+	return sum;
 }
-void vivodmaxValueIn(int arr[], int n)
+
+double Avg(const int arr[], const int n)
 {
-    for (int i = 0; i < 1; i++)
-    {
-        cout << "Максимальное знач. массива = " << maxValueIn(arr, n) << " ";
-    }
-    cout << endl;
+	return (double)Sum(arr, n) / n;
 }
-int main()
+double Avg(const double arr[], const int n)
 {
-    setlocale(LC_ALL, "");
-    srand(time(NULL));
-    const int n = 5;
-    int arr[n];
-    int minRand, maxRand;
-    do
-    {
-        cout << " Введите минимально возможное случайное число:"; cin >> minRand;
-        cout << " Введите максимально возможное случайное число:"; cin >> maxRand;
-        if (minRand == maxRand) cout << "Пределы диапазона не должны совпадать" << endl;
-    } while (minRand == maxRand);
-    if (maxRand < minRand)
-    {
-        int buffer = minRand;
-        minRand = maxRand;
-        maxRand = buffer;
-    }
-    FillRand(arr, n, minRand, maxRand);
-    Print(arr, n);
-    Sum(arr, n);
-    vivodSum(arr, n);
-    Avg(arr, n);
-    vivodAvg(arr, n);
-    minValueIn(arr, n);
-    vivodminValueIn(arr, n);
-    maxValueIn(arr, n);
-    vivodmaxValueIn(arr, n);
-    return 0;
+	return (double)Sum(arr, n) / n;
+}
+
+int minValueIn(const int arr[], const int n)
+{
+	int min = arr[0];
+	for (int i = 1; i < n; i++)
+	{
+		if (arr[i] < min)
+		min = arr[i];
+
+	}
+	return min;
+}
+int minValueIn(const double arr[], const int n)
+{
+	int min = arr[0];
+	for (int i = 1; i < n; i++)
+	{
+		if (arr[i] < min)
+			min = arr[i];
+
+	}
+	return min;
+}
+
+int maxValueIn(const int arr[], const int n)
+{
+	int max = arr[0];
+	for (int i = 1; i < n; i++)
+	{
+		if (arr[i] > max)
+			max = arr[i];
+
+	}
+	return max;
+}
+int maxValueIn(const double arr[], const int n)
+{
+	int max = arr[0];
+	for (int i = 1; i < n; i++)
+	{
+		if (arr[i] > max)
+			max = arr[i];
+
+	}
+	return max;
+}
+
+void shiftleft(int arr[], const int n)
+{
+	int number_of_shifts;
+	cout << "На сколько элементов сдвинуть влево массив:"; cin >> number_of_shifts;
+	for (int i = 0; i < number_of_shifts; i++)
+	{
+		int buffer = arr[0];
+		for (int  j = 0; j < n - 1; j++)
+		{
+			arr[j] = arr[j + 1];
+		}
+		arr[n - 1] = buffer;
+		
+	}
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << tab;
+	}
+}
+
+void shiftleft(double arr[], const int n)
+{
+	int number_of_shifts;
+	cout << "На сколько элементов сдвинуть влево массив:"; cin >> number_of_shifts;
+	for (int i = 0; i < number_of_shifts; i++)
+	{
+		int buffer = arr[0];
+		for (int j = 0; j < n - 1; j++)
+		{
+			arr[j] = arr[j + 1];
+		}
+		arr[n - 1] = buffer;
+
+	}
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << tab;
+	}
+}
+
+void shiftRight(int arr[], const int n)
+{
+	int number_of_shifts;
+	cout << "На сколько элементов сдвинуть массив вправо:"; cin >> number_of_shifts;
+	for (int i = 0; i < number_of_shifts; i++)
+	{
+		int buffer = arr[n - 1];
+		for (int i = n - 1; i > 0 ; i--)
+		{
+			arr[i] = arr[i - 1];
+		}
+		arr[0] = buffer;
+	}
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << tab;
+	}
+}
+void shiftRight(double arr[], const int n)
+{
+	int number_of_shifts;
+	cout << "На сколько элементов сдвинуть массив вправо:"; cin >> number_of_shifts;
+	for (int i = 0; i < number_of_shifts; i++)
+	{
+		int buffer = arr[n - 1];
+		for (int i = n - 1; i > 0; i--)
+		{
+			arr[i] = arr[i - 1];
+		}
+		arr[0] = buffer;
+	}
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << tab;
+	}
 }
